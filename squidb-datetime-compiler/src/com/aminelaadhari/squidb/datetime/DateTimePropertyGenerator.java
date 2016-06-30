2bc575b08479309df4a821bd79b58e0a041b6275
Amine Laadhari <amine.laadhari@gmail.com>
2016-06-30 18:28:13 +0200

    First commit

---
+++ squidb-datetime-compiler/src/com/aminelaadhari/squidb/datetime/DateTimePropertyGenerator.java
@@ -0,0 +1,49 @@
+package com.aminelaadhari.squidb.datetime;
+
+import com.yahoo.aptutils.model.DeclaredTypeName;
+import com.yahoo.aptutils.utils.AptUtils;
+import com.yahoo.aptutils.writer.JavaFileWriter;
+import com.yahoo.squidb.processor.data.ModelSpec;
+import com.yahoo.squidb.processor.plugins.defaults.properties.generators.BasicLongPropertyGenerator;
+
+import java.io.IOException;
+import java.util.Set;
+
+import javax.lang.model.element.VariableElement;
+
+public class DateTimePropertyGenerator extends BasicLongPropertyGenerator {
+    private static final DeclaredTypeName JODA_DATETIME = new DeclaredTypeName("org.joda.time.DateTime");
+    private final DeclaredTypeName fieldType;
+
+    public DateTimePropertyGenerator(ModelSpec<?> modelSpec, VariableElement field, DeclaredTypeName fieldType, AptUtils utils) {
+        super(modelSpec, field, utils);
+        this.fieldType = fieldType;
+    }
+
+    // Add imports required by this property here
+    @Override
+    protected void registerAdditionalImports(Set<DeclaredTypeName> imports) {
+        super.registerAdditionalImports(imports);
+        imports.add(JODA_DATETIME);
+    }
+
+    // Defines the type passed/returned in get/set
+    @Override
+    public DeclaredTypeName getTypeForAccessors() {
+        return fieldType;
+    }
+
+    // Generates getter implementation using the static helper class from earlier
+    @Override
+    protected void writeGetterBody(JavaFileWriter writer) throws IOException {
+        writer.writeStringStatement("Long instant = this.get(" + propertyName + ")");
+        writer.writeStringStatement("return instant == null ? null : new DateTime(instant)");
+    }
+
+    // Generates setter implementation using the static helper class from earlier
+    @Override
+    protected void writeSetterBody(JavaFileWriter writer, String argName) throws IOException {
+        writer.writeStringStatement("this.set(" + propertyName + ", " + argName + " == null ? null : " + argName + ".getMillis())");
+        writer.writeStringStatement("return this");
+    }
+}
---
+++ squidb-datetime-plugin/src/com/aminelaadhari/squidb/datetime/DateTimePlugin.java
@@ -0,0 +1,28 @@
+package com.aminelaadhari.squidb.datetime;
+
+import com.yahoo.aptutils.model.DeclaredTypeName;
+import com.aminelaadhari.squidb.datetime.annotations.DateTimeColumn;
+import com.yahoo.squidb.processor.data.ModelSpec;
+import com.yahoo.squidb.processor.plugins.Plugin;
+import com.yahoo.squidb.processor.plugins.PluginEnvironment;
+
+import javax.lang.model.element.VariableElement;
+
+public class DateTimePlugin extends Plugin {
+
+    private static final DeclaredTypeName JODA_DATETIME = new DeclaredTypeName("org.joda.time.DateTime");
+
+    public DateTimePlugin(ModelSpec<?> modelSpec, PluginEnvironment pluginEnv) {
+        super(modelSpec, pluginEnv);
+    }
+
+    @Override
+    public boolean processVariableElement(VariableElement field, DeclaredTypeName fieldType) {
+        if (field.getAnnotation(DateTimeColumn.class) == null ||
+                !JODA_DATETIME.equals(fieldType)) {
+            return false;
+        }
+        modelSpec.addPropertyGenerator(new DateTimePropertyGenerator(modelSpec, field, fieldType, utils));
+        return true;
+    }
+}
